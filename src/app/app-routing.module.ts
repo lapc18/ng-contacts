6373606f6548fb61bef5555d0a28d78ad52ae663
lapc18 <lapc18@outlook.com>
2023-02-11 22:47:00 -0400

    feat: setup initial changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AppRoutes } from './core/enums/app-routes.enum';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: AppRoutes.DEFAULT, component: AppComponent, pathMatch: 'full' },
+  { path: AppRoutes.AUTH, loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) }, 
+  { path: AppRoutes.PROFILE, loadChildren: () => import('./features/profile/profile.module').then(m => m.ProfileModule) },
+  { path: AppRoutes.HOME, loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './features/home/home.module';
+import { AuthModule } from './features/auth/auth.module';
+import { ProfileModule } from './features/profile/profile.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    HomeModule,
+    AuthModule,
+    ProfileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
