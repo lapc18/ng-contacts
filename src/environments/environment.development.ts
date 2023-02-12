ef1707c25a6c89f5ce8f65c9521f101b473bc01a
lapc18 <lapc18@outlook.com>
2023-02-11 22:46:30 -0400

    feat: add env files, features and shared modules

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  api: {
+    auth: "http://localhost:9903/api/auth",
+    users: "http://localhost:9903/api/users",
+    contacts: "http://localhost:9903/api/contacts"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  api: {
+    auth: "http://localhost:9903/api/auth",
+    users: "http://localhost:9903/api/users",
+    contacts: "http://localhost:9903/api/contacts"
+  }
+};
